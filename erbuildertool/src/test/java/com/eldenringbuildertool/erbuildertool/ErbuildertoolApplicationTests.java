de0d8fe51edc6125ef9ca740648b59e737e0a903
Andrea <andrea.mainardi2002@gmail.com>
2022-03-10 18:12:04 +0100

    Commit

---
+++ erbuildertool/src/test/java/com/eldenringbuildertool/erbuildertool/ErbuildertoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eldenringbuildertool.erbuildertool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErbuildertoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
