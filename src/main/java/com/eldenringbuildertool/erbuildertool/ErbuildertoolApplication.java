5020cf3d7dd446271c5134fcc4dcce382cf72324
Andrea <andrea.mainardi2002@gmail.com>
2022-03-10 19:55:12 +0100

    first commit

---
+++ src/main/java/com/eldenringbuildertool/erbuildertool/ErbuildertoolApplication.java
@@ -0,0 +1,13 @@
+package com.eldenringbuildertool.erbuildertool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErbuildertoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErbuildertoolApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/eldenringbuildertool/erbuildertool/model/Gear.java
@@ -0,0 +1,119 @@
+package com.eldenringbuildertool.erbuildertool.model;
+
+public class Gear {
+	String file;
+	String name;
+	Double phy;
+	Double strike;
+	Double slash;
+	Double pierce;
+	Double mag;
+	Double fir;
+	Double lit;
+	Double hol;
+	Double immunity;
+	Double robustness;
+	Double focus;
+	Double vitality;
+	Double poise;
+	Double wgt;
+	
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getPhy() {
+		return phy;
+	}
+	public void setPhy(Double phy) {
+		this.phy = phy;
+	}
+	public Double getStrike() {
+		return strike;
+	}
+	public void setStrike(Double strike) {
+		this.strike = strike;
+	}
+	public Double getSlash() {
+		return slash;
+	}
+	public void setSlash(Double slash) {
+		this.slash = slash;
+	}
+	public Double getPierce() {
+		return pierce;
+	}
+	public void setPierce(Double pierce) {
+		this.pierce = pierce;
+	}
+	public Double getMag() {
+		return mag;
+	}
+	public void setMag(Double mag) {
+		this.mag = mag;
+	}
+	public Double getFir() {
+		return fir;
+	}
+	public void setFir(Double fir) {
+		this.fir = fir;
+	}
+	public Double getLit() {
+		return lit;
+	}
+	public void setLit(Double lit) {
+		this.lit = lit;
+	}
+	public Double getHol() {
+		return hol;
+	}
+	public void setHol(Double hol) {
+		this.hol = hol;
+	}
+	public Double getImmunity() {
+		return immunity;
+	}
+	public void setImmunity(Double immunity) {
+		this.immunity = immunity;
+	}
+	public Double getRobustness() {
+		return robustness;
+	}
+	public void setRobustness(Double robustness) {
+		this.robustness = robustness;
+	}
+	public Double getFocus() {
+		return focus;
+	}
+	public void setFocus(Double focus) {
+		this.focus = focus;
+	}
+	public Double getVitality() {
+		return vitality;
+	}
+	public void setVitality(Double vitality) {
+		this.vitality = vitality;
+	}
+	public Double getPoise() {
+		return poise;
+	}
+	public void setPoise(Double poise) {
+		this.poise = poise;
+	}
+	public Double getWgt() {
+		return wgt;
+	}
+	public void setWgt(Double wgt) {
+		this.wgt = wgt;
+	}
+
+	
+}
